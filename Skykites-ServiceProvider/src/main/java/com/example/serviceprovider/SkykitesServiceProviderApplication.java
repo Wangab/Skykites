658c568117faf6120ab2cfe5bef251a529fc8607
wanganbang@global-hgroup.com <wanganbang@global-hgroup.com>
2022-05-20 22:04:03 +0800

    添加初始化代码

--- Skykites-ServiceProvider/src/main/java/com/example/serviceprovider/SkykitesServiceProviderApplication.java
+++ Skykites-ServiceProvider/src/main/java/com/example/serviceprovider/SkykitesServiceProviderApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkykitesServiceProviderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkykitesServiceProviderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SkykitesServiceProviderApplication.class, args);
+    }
 
 }
